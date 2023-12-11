dad74c853adea56d0850e9f2c94f48d0b8571a8f
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-12-11 20:24:01 +0530

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:weather/weather.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:weather_app_tutorial/consts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final WeatherFactory _wf = WeatherFactory(OPENWEATHER_API_KEY);
+
+  Weather? _weather;
+  String _cityName = 'Mumbai'; // Default city name
+
+  @override
+  void initState() {
+    super.initState();
+    _getWeather(_cityName);
+  }
+
+  Future<void> _getWeather(String city) async {
+    Weather? weather = await _wf.currentWeatherByCityName(city);
+    if (weather != null) {
+      setState(() {
+        _weather = weather;
+      });
+    }
+  }
+
+  Future<void> _getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Handle if location services are not enabled
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Handle if location permissions are denied
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Handle if location permissions are permanently denied
+      return;
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+
+    if (placemarks.isNotEmpty) {
+      setState(() {
+        _cityName = placemarks[0].locality ?? 'Unknown';
+      });
+      _getWeather(_cityName);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false, // To avoid bottom overflow error
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+            onSubmitted: (value) {
+              setState(() {
+                _cityName = value;
+              });
+              _getWeather(_cityName);
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter city name',
+              suffixIcon: IconButton(
+                onPressed: () {
+                  _getLocation();
+                },
+                icon: const Icon(Icons.location_searching),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    if (_weather == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).height,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _locationHeader(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.06,
+          ),
+          _dateTimeInfo(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.03,
+          ),
+          _weatherIcon(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.01,
+          ),
+          _currentTemp(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.01,
+          ),
+          _extraInfo(),
+        ],
+      ),
+    );
+  }
+
+  Widget _locationHeader() {
+    return Text(
+      _weather?.areaName ?? "",
+      style: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _dateTimeInfo() {
+    DateTime now = _weather!.date!;
+    return Column(
+      children: [
+        Text(
+          DateFormat("h:mm a").format(now),
+          style: const TextStyle(
+            fontSize: 35,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              DateFormat("EEEE").format(now),
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              "  ${DateFormat("d.m.y").format(now)}",
+              style: const TextStyle(
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _weatherIcon() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: MediaQuery.sizeOf(context).height * 0.20,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  "http://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png"),
+            ),
+          ),
+        ),
+        Text(
+          _weather?.weatherDescription ?? "",
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _currentTemp() {
+    return Text(
+      "${_weather?.temperature?.celsius?.toStringAsFixed(0)}° C",
+      style: const TextStyle(
+        color: Colors.black,
+        fontSize: 90,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _extraInfo() {
+    return Container(
+      height: MediaQuery.sizeOf(context).height * 0.15,
+      width: MediaQuery.sizeOf(context).width * 0.80,
+      decoration: BoxDecoration(
+        color: Colors.deepPurpleAccent,
+        borderRadius: BorderRadius.circular(
+          20,
+        ),
+      ),
+      padding: const EdgeInsets.all(
+        8.0,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Max: ${_weather?.tempMax?.celsius?.toStringAsFixed(0)}° C",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                "Min: ${_weather?.tempMin?.celsius?.toStringAsFixed(0)}° C",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Wind: ${_weather?.windSpeed?.toStringAsFixed(0)}m/s",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                "Humidity: ${_weather?.humidity?.toStringAsFixed(0)}%",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
